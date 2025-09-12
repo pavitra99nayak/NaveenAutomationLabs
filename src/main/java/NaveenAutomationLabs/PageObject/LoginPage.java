cc4a60e3b102cc91c39b992d9f5d69d3e9500170
Pavitra <pavitranayak832@gmail.com>
2025-09-12 23:37:43 +0530

    LoginModule Commit

--- src/main/java/NaveenAutomationLabs/PageObject/LoginPage.java
+++ src/main/java/NaveenAutomationLabs/PageObject/LoginPage.java
@@ -1,14 +1,18 @@
 package NaveenAutomationLabs.PageObject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
+import NaveenAutomationLabs.AbstractMethods.AbstractMethod;
+
+public class LoginPage extends AbstractMethod {
 	WebDriver driver;
 	public LoginPage(WebDriver driver)
 	{
+		super(driver);
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
 	}
@@ -17,10 +21,31 @@ public class LoginPage {
 	WebElement myActBtn;
 	@FindBy(xpath="(//ul[@class='dropdown-menu dropdown-menu-right']//li)[2]")
 	WebElement loginBtn;
-	
-	public void loginWithValidCreds()
+	@FindBy(id="input-email")
+	WebElement email;
+	@FindBy(id="input-password")
+	WebElement password;
+	@FindBy(xpath="//input[@value='Login']")
+	WebElement LoginBtn;
+	@FindBy(xpath="//div[@class='pull-right']")
+	WebElement continueBtn;
+	@FindBy(xpath="(//span[@class='hidden-xs hidden-sm hidden-md'])[3]")
+	WebElement myAct2;
+	@FindBy(xpath="(//ul[@class='dropdown-menu dropdown-menu-right']//li)[5]")
+	WebElement logout;
+	public void loginWithValidCreds(String Email, String Password)
 	{
 		myActBtn.click();
 		loginBtn.click();
+		email.sendKeys(Email);
+		password.sendKeys(Password);
+		LoginBtn.click();
+	}
+	
+	public void logOut()
+	{
+		WaitForVisibleElement(By.xpath("(//span[@class='hidden-xs hidden-sm hidden-md'])[3]"));
+		myAct2.click();
+		logout.click();
 	}
 }
--- src/test/java/NaveenAutomationLabs/ScenariosTest/LoginTest.java
+++ src/test/java/NaveenAutomationLabs/ScenariosTest/LoginTest.java
@@ -1,15 +1,31 @@
 package NaveenAutomationLabs.ScenariosTest;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import NaveenAutomationLabs.BaseTest.BaseTest;
+import NaveenAutomationLabs.PageObject.LoginPage;
 
 public class LoginTest extends BaseTest{
 	
-	@Test
-	public void ValidLogin()
+	@Test(dataProvider="dataset")
+	public void ValidLogin(HashMap<String,String> input)
 	{
-		
+		LoginPage login = new LoginPage(driver);
+		login.loginWithValidCreds(input.get("Email"), input.get("Password"));
+		login.logOut();
+	}
+	
+	@DataProvider
+	public Object[][] dataset() throws IOException
+	{
+		String FilePath = System.getProperty("user.dir")+"/src/test/java/NaveenAutomationLabs/BaseTest/RegisterDate.json";
+		List<HashMap<String,String>> input = dataReader(FilePath);
+		return new Object[][] {{input.get(0)},{input.get(1)}};
 	}
 
 }
